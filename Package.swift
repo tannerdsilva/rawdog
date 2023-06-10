7371297b1d3d9ebfa666dbca5db309193bd59954
Tanner Silva <t@tannersilva.com>
2023-06-10 12:27:10 -0500

    primary product is now named RAW

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "rawdog",
+            name: "RAW",
             targets: ["rawdog"]),
     ],
     targets: [
