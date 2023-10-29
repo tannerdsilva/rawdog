94c912f08fcbaa286d05c3cea0df987d9d283081
Tanner Silva <t@tannersilva.com>
2023-10-28 21:12:57 -0500

    updated platform support for macos

--- Package.swift
+++ Package.swift
@@ -1,24 +1,22 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
 let package = Package(
-    name: "rawdog",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "RAW",
-            targets: ["RAW"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "RAW",
+	name: "rawdog",
+	platforms:[
+		.macOS(.v11)
+	],
+	products: [
+		.library(
+			name: "RAW",
+			targets: ["RAW"]),
+	],
+	targets: [
+		.target(
+			name: "RAW",
 			dependencies: ["CRAW"]),
 		.target(
 			name: "CRAW"),
 		.testTarget(name:"PrimitiveTests", dependencies:["RAW"]),
-    ]
+	]
 )
