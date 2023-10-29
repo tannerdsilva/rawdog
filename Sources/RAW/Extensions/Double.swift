ed64f9d7bba1cffb885aa86fb5b29b3b8be17be1
Tanner Silva <t@tannersilva.com>
2023-10-28 21:22:04 -0500

    removed float16 and float32 - now only implementing float

--- Sources/RAW/Extensions/Double.swift
+++ Sources/RAW/Extensions/Double.swift
@@ -20,30 +20,8 @@ extension Double:RAW_encodable, RAW_decodable {
 	}
 }
 
-extension Float16:RAW_encodable, RAW_decodable {
-	/// retrieves the raw IEEE 754 representation of the float16.
-	public func asRAW_val<R>(_ valFunc:(RAW) throws -> R) rethrows -> R {
-		return try withUnsafePointer(to:self.bitPattern) { ptr in
-			return try ptr.withMemoryRebound(to:UInt8.self, capacity:MemoryLayout<UInt16>.size) { bytePtr in
-				return try valFunc(RAW(bytePtr, MemoryLayout<UInt16>.size))
-			}
-		}
-	}
-	
-	/// intializes a float16 from a raw IEEE 754 representation in memory.
-	public init?(RAW_size:UInt64, RAW_data:UnsafeRawPointer?) {
-		guard (RAW_size == UInt64(MemoryLayout<UInt16>.size)) else {
-			return nil
-		}
-		guard (RAW_data != nil) else {
-			return nil
-		}
-		self = Float16(bitPattern:RAW_data!.load(as:UInt16.self))
-	}
-}
-
-extension Float32:RAW_encodable, RAW_decodable {
-	/// retrieves the raw IEEE 754 representation of the float32.
+extension Float:RAW_encodable, RAW_decodable {
+	/// retrieves the raw IEEE 754 representation of the float.
 	public func asRAW_val<R>(_ valFunc:(RAW) throws -> R) rethrows -> R {
 		return try withUnsafePointer(to:self.bitPattern) { ptr in
 			return try ptr.withMemoryRebound(to:UInt8.self, capacity:MemoryLayout<UInt32>.size) { bytePtr in
@@ -60,6 +38,6 @@ extension Float32:RAW_encodable, RAW_decodable {
 		guard (RAW_data != nil) else {
 			return nil
 		}
-		self = Float32(bitPattern:RAW_data!.load(as:UInt32.self))
+		self = Float(bitPattern:RAW_data!.load(as:UInt32.self))
 	}
 }
