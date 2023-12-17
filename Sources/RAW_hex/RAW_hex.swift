8368de34fce1ff7fbbc32d2d4ffb52e88ed14f33
tannerdsilva <t@tannersilva.com>
2023-12-17 17:12:38 -0600

    exploring new ideas with hex encoding

--- Sources/RAW_hex/RAW_hex.swift
+++ Sources/RAW_hex/RAW_hex.swift
@@ -1,13 +1,122 @@
 import CRAW_hex
 import RAW
 
-enum Error: Swift.Error {
+public enum Error: Swift.Error {
 	/// general error that is thrown when a function returns false
 	case hexDecodeFailed
+	/// general error that is thrown when a function returns false
 	case hexEncodeFailed
+	/// thrown when the hex string is not a valid hex string.
+	case invalidHexCharacter(UInt8)
 }
 
-public func hex_decode(_ hex:String, to:[UInt8].Type) throws -> [UInt8] {
+public enum HexValue:UInt8, RAW_encodable {
+
+	case zero = 0x30
+	case one = 0x31
+	case two = 0x32
+	case three = 0x33
+	case four = 0x34
+	case five = 0x35
+	case six = 0x36
+	case seven = 0x37
+	case eight = 0x38
+	case nine = 0x39
+	case a = 0x61
+	case b = 0x62
+	case c = 0x63
+	case d = 0x64
+	case e = 0x65
+	case f = 0x66
+
+	/// initialize a hex value from a character value.
+	/// - throws: `Error.invalidHexCharacter` if the character is not a valid hex character.
+	init(_ char:Character) throws {
+		switch char {
+		case "0":
+			self = .zero
+		case "1":
+			self = .one
+		case "2":
+			self = .two
+		case "3":
+			self = .three
+		case "4":
+			self = .four
+		case "5":
+			self = .five
+		case "6":
+			self = .six
+		case "7":
+			self = .seven
+		case "8":
+			self = .eight
+		case "9":
+			self = .nine
+		case "a", "A":
+			self = .a
+		case "b", "B":
+			self = .b
+		case "c", "C":
+			self = .c
+		case "d", "D":
+			self = .d
+		case "e", "E":
+			self = .e
+		case "f", "F":
+			self = .f
+		default:
+			throw Error.invalidHexCharacter(char.asciiValue!)
+		}
+	}
+
+	/// initialize a hex value from a c character byte
+	/// - throws: `Error.invalidHexCharacter` if the character is not a valid hex character.
+	init(_ char:CChar) throws {
+		switch char {
+		case 0x30:
+			self = .zero
+		case 0x31:
+			self = .one
+		case 0x32:
+			self = .two
+		case 0x33:
+			self = .three
+		case 0x34:
+			self = .four
+		case 0x35:
+			self = .five
+		case 0x36:
+			self = .six
+		case 0x37:
+			self = .seven
+		case 0x38:
+			self = .eight
+		case 0x39:
+			self = .nine
+		case 0x61, 0x41:
+			self = .a
+		case 0x62, 0x42:
+			self = .b
+		case 0x63, 0x43:
+			self = .c
+		case 0x64, 0x44:
+			self = .d
+		case 0x65, 0x45:
+			self = .e
+		case 0x66, 0x46:
+			self = .f
+		default:
+			throw Error.invalidHexCharacter(UInt8(bitPattern:char))
+		}
+	}
+}
+
+public typealias HexEncoding = [HexValue]
+
+/// decode a hex string into a buffer of bytes.
+/// - parameter hex: the hex string to decode.
+public func hex_decode(_ hex:[HexValue]) throws -> [UInt8] {
 	let hexDataSize = hex_data_size(hex.count)
 	let decodedData = try [UInt8](unsafeUninitializedCapacity:hexDataSize, initializingWith: { (buffer, count) in
 		count = hexDataSize
@@ -45,6 +154,7 @@ public func hex_encode<I>(_ encodable_input:I) throws -> String where I:RAW_enco
 		return String(cString:buffer.baseAddress!)
 	}
 }
+
 public func hex_encode<O>(_ data:O) throws -> String where O:RAW_val {
 	let hexStrSize = hex_str_size(data.RAW_size)
 	let buffer = UnsafeMutableBufferPointer<CChar>.allocate(capacity:hexStrSize)
