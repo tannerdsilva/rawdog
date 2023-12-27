c2525faf13086bf500bca28a6b2286a2ac99672b
tannerdsilva <t@tannersilva.com>
2023-12-27 13:15:16 -0600

    wip continued

---
+++ Sources/RAW_hex/HexValue.swift
@@ -0,0 +1,36 @@
+/// represents one of the 16 possible values in a hexadecimal number
+public enum Value {
+
+	case zero
+
+	case one
+	
+	case two
+	
+	case three
+
+	case four
+
+	case five
+
+	case six
+
+	case seven
+
+	case eight
+
+	case nine	
+
+	case a
+
+	case b
+
+	case c
+
+	case d
+
+	case e
+
+	case f
+
+}
